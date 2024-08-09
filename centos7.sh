97627b1e12f8d2737a773c98e39800fcad968daf
Sensato-CW <jbuice@gocloudwave.com>
2024-08-09 17:54:56 -0400

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+echo "Backing up repository settings."
 sudo cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
 
-# Backup the original repo file
-sudo cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
-
+echo "Updating the repository."
 # Update the [base] repository
 sudo sed -i 's|^mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra|' /etc/yum.repos.d/CentOS-Base.repo
 sudo sed -i 's|^#baseurl=http://mirror.centos.org/centos/$releasever/os/$basearch/|baseurl=http://vault.centos.org/centos/$releasever/os/$basearch/|' /etc/yum.repos.d/CentOS-Base.repo
@@ -19,13 +18,16 @@ sudo sed -i 's|^#baseurl=http://mirror.centos.org/centos/$releasever/extras/$bas
 # Disable the CentOS Plus repo (optional, only if you don't need it)
 sudo sed -i 's|^enabled=1|enabled=0|' /etc/yum.repos.d/CentOS-Base.repo
 
-
+echo "Performing yum update."
 sudo yum clean all
 sudo yum update -y
 sudo yum makecache
 
+echo "Installing dependencies for CloudWave HIDS."
 sudo yum --enablerepo=base,updates,extras install -y perl gcc make zlib-devel pcre2-devel libevent-devel curl wget git
 
+echo "Retrieving Installer."
 wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash -s -- -y
 
+echo "Installing HIDS agent."
 sudo yum install ossec-hids-agent
