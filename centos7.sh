43bdbe05f9e56a054ce0d08e76d0bec38990fdd1
Sensato-CW <jbuice@gocloudwave.com>
2024-08-09 15:41:51 -0400

    Create centos7.sh

---
+++ centos7.sh
@@ -0,0 +1,24 @@
+sudo cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+
+sudo sed -i 's|mirrorlist=http://mirrorlist.centos.org/\?release=$releasever&arch=$basearch&repo=os&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/\?release=$releasever&arch=$basearch&repo=os&infra=$infra|' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo sed -i 's|#baseurl=http://vault.centos.org/centos/$releasever/os/$basearch/|baseurl=http://vault.centos.org/centos/$releasever/os/$basearch/|' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo sed -i 's|mirrorlist=http://mirrorlist.centos.org/\?release=$releasever&arch=$basearch&repo=updates&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/\?release=$releasever&arch=$basearch&repo=updates&infra=$infra|' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo sed -i 's|#baseurl=http://vault.centos.org/centos/$releasever/updates/$basearch/|baseurl=http://vault.centos.org/centos/$releasever/updates/$basearch/|' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo sed -i 's|mirrorlist=http://mirrorlist.centos.org/\?release=$releasever&arch=$basearch&repo=extras&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/\?release=$releasever&arch=$basearch&repo=extras&infra=$infra|' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo sed -i 's|#baseurl=http://vault.centos.org/centos/$releasever/extras/$basearch/|baseurl=http://vault.centos.org/centos/$releasever/extras/$basearch/|' /etc/yum.repos.d/CentOS-Base.repo
+
+
+sudo yum clean all
+sudo yum update -y
+sudo yum makecache
+
+sudo yum --enablerepo=base,updates,extras install perl gcc make zlib-devel pcre2-devel libevent-devel curl wget git
+
+wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
+
+sudo yum install ossec-hids-agent
