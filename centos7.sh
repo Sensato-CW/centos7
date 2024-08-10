8879e11e082d9da3f0267650a08f87615587c583
Sensato-CW <jbuice@gocloudwave.com>
2024-08-10 12:20:50 -0400

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
+# Backup existing repo settings
 echo "Backing up repository settings."
 sudo cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
 
+# Update repo URLs
 echo "Updating the repository."
-# Update the [base] repository
 sudo sed -i 's|^mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra|' /etc/yum.repos.d/CentOS-Base.repo
 sudo sed -i 's|^#baseurl=http://mirror.centos.org/centos/$releasever/os/$basearch/|baseurl=http://vault.centos.org/centos/$releasever/os/$basearch/|' /etc/yum.repos.d/CentOS-Base.repo
 
-# Update the [updates] repository
 sudo sed -i 's|^mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra|' /etc/yum.repos.d/CentOS-Base.repo
 sudo sed -i 's|^#baseurl=http://mirror.centos.org/centos/$releasever/updates/$basearch/|baseurl=http://vault.centos.org/centos/$releasever/updates/$basearch/|' /etc/yum.repos.d/CentOS-Base.repo
 
-# Update the [extras] repository
 sudo sed -i 's|^mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra|#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra|' /etc/yum.repos.d/CentOS-Base.repo
 sudo sed -i 's|^#baseurl=http://mirror.centos.org/centos/$releasever/extras/$basearch/|baseurl=http://vault.centos.org/centos/$releasever/extras/$basearch/|' /etc/yum.repos.d/CentOS-Base.repo
 
-# Disable the CentOS Plus repo (optional, only if you don't need it)
 sudo sed -i 's|^enabled=1|enabled=0|' /etc/yum.repos.d/CentOS-Base.repo
 
+# Perform yum update
 echo "Performing yum update."
 sudo yum clean all
 sudo yum update -y
 sudo yum makecache
 
+# Install dependencies
 echo "Installing dependencies for CloudWave HIDS."
 sudo yum --enablerepo=base,updates,extras install -y perl gcc make zlib-devel pcre2-devel libevent-devel curl wget git expect
 
+# Automate wget command using expect
 echo "Retrieving Installer."
 expect <<- EOF
 spawn wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
@@ -34,5 +35,6 @@ expect "Do you agree to these terms?" { send "yes\r" }
 expect eof
 EOF
 
+# Install OSSEC HIDS agent
 echo "Installing HIDS agent."
-sudo yum install ossec-hids-agent
+sudo yum install -y ossec-hids-agent
